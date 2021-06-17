cec4062a0d337c5c9e6d9509d5d174af9c79ac4f
Jay Kannaiyan <jay.kannaiyan@ibm.com>
2021-06-17 14:01:42 -0500

    adding sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
         `gatsby-transformer-remark`,
+        `gatsby-plugin-sitemap`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
