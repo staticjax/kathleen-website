d397f8868895ffe6b7da9fe0ec9de3a5d072db37
Kathleen Broussard <rkbroussard@pm.me>
2026-08-25 11:14:00 -0400

    Italicize journal names in searchable research list, make thumbnails clickable to DOI

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -16,6 +16,22 @@ export const query = graphql`
   }
 `
 
+function formatExcerpt(excerpt) {
+  if (!excerpt) return null
+  const yearMatch = excerpt.match(/\b(19|20)\d{2}\b/)
+  if (yearMatch) {
+    const yearIdx = excerpt.indexOf(yearMatch[0])
+    const journalPart = excerpt.slice(0, yearIdx).trim()
+    const yearPart = excerpt.slice(yearIdx).trim()
+    return (
+      <>
+        <em>{journalPart}</em>{yearPart ? ` ${yearPart}` : ''}
+      </>
+    )
+  }
+  return <em>{excerpt}</em>
+}
+
 export default class Blog extends React.Component {
   render() {
     let display_posts = _.orderBy(
@@ -35,17 +51,28 @@ export default class Blog extends React.Component {
                 <div className='post-inside'>
                   <div className='post-column'>
                     {_.get(post, 'frontmatter.thumb_img_path', null) && (
-                      <Link
-                        className='post-thumbnail'
-                        to={withPrefix(_.get(post, 'url', null))}
-                      >
-                        <img
-                          src={withPrefix(
-                            _.get(post, 'frontmatter.thumb_img_path', null)
-                          )}
-                          alt={_.get(post, 'frontmatter.thumb_img_alt', null)}
-                        />
-                      </Link>
+                      _.get(post, 'frontmatter.external_url', null) ? (
+                        <a className='post-thumbnail' href={_.get(post, 'frontmatter.external_url', null)} target="_blank" rel="noopener">
+                          <img
+                            src={withPrefix(
+                              _.get(post, 'frontmatter.thumb_img_path', null)
+                            )}
+                            alt={_.get(post, 'frontmatter.thumb_img_alt', null)}
+                          />
+                        </a>
+                      ) : (
+                        <Link
+                          className='post-thumbnail'
+                          to={withPrefix(_.get(post, 'url', null))}
+                        >
+                          <img
+                            src={withPrefix(
+                              _.get(post, 'frontmatter.thumb_img_path', null)
+                            )}
+                            alt={_.get(post, 'frontmatter.thumb_img_alt', null)}
+                          />
+                        </Link>
+                      )
                     )}
                   </div>
                   <div className='post-wide-column'>
@@ -67,7 +94,7 @@ export default class Blog extends React.Component {
                     </header>
                     {_.get(post, 'frontmatter.excerpt', null) && (
                       <div className='post-content'>
-                        <p>{_.get(post, 'frontmatter.excerpt', null)}</p>
+                        <p>{formatExcerpt(_.get(post, 'frontmatter.excerpt', null))}</p>
                       </div>
                     )}
                     <footer className='post-meta'>
