bbecd10d6a81a6d40745ba5f8d254fd001a31e75
Kathleen Broussard <rkbroussard@pm.me>
2026-08-25 10:29:52 -0400

    Improved design: hero title PhD, zoomed-out headshot, About-first nav, Teaching/Public Impact/Mentorship updates, warm palette

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -50,12 +50,18 @@ export default class SectionPosts extends React.Component {
                   <div className='post-wide-column'>
                     <header className='post-header'>
                       <h3 className='post-title'>
-                        <Link
-                          to={withPrefix(_.get(post, 'url', null))}
-                          rel='bookmark'
-                        >
-                          {_.get(post, 'frontmatter.title', null)}
-                        </Link>
+                        {_.get(post, 'frontmatter.external_url', null) ? (
+                          <a href={_.get(post, 'frontmatter.external_url', null)} target="_blank" rel="noopener" >
+                            {_.get(post, 'frontmatter.title', null)}
+                          </a>
+                        ) : (
+                          <Link
+                            to={withPrefix(_.get(post, 'url', null))}
+                            rel='bookmark'
+                          >
+                            {_.get(post, 'frontmatter.title', null)}
+                          </Link>
+                        )}
                       </h3>
                     </header>
                     {_.get(post, 'frontmatter.excerpt', null) && (
@@ -74,6 +80,9 @@ export default class SectionPosts extends React.Component {
                           '%B %d, %Y'
                         )}
                       </time>
+                      {_.get(post, 'frontmatter.external_url', null) && (
+                        <span> · <a className="post-doi-link" href={_.get(post, 'frontmatter.external_url', null)} target="_blank" rel="noopener">Read study ↗</a></span>
+                      )}
                     </footer>
                   </div>
                 </div>
